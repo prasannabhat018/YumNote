4e3e51a3f269d8489e4a572e564c765013dee059
Prasanna Bhat <prasannabhat018@gmail.com>
2025-08-02 18:50:54 +0530

    Initial Commit

---
+++ YumNote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YumNote
+//
+//  Created by Prasanna Bhat on 02/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YumNote/YumNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumNoteApp.swift
+//  YumNote
+//
+//  Created by Prasanna Bhat on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct YumNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
